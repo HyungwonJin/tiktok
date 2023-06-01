aebecb01b7e229c276c4d548f089ccd4c77ab79c
HyungwonJin <hwjin92@gmail.com>
2023-06-01 21:01:54 +0900

    feat: create Username Screen

---
+++ lib/features/authentication/username_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class UsernameScreen extends StatelessWidget {
+  const UsernameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Sign up",
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: Sizes.size40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            Text(
+              "Create username",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            Text(
+              "You can always change this later.",
+              style: TextStyle(
+                fontSize: Sizes.size20,
+                color: Colors.black54,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
