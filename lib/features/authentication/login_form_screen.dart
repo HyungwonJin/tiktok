8f0f69e97e1660fee8253fc5f10bb02f3ef7c3e7
HyungwonJin <hwjin92@gmail.com>
2023-06-03 13:07:51 +0900

    feat: Add validation and login navigator

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -19,7 +20,11 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        print(formData);
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const InterestsSCreen(),
+          ),
+        );
       }
     }
   }
@@ -40,26 +45,57 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
               children: [
                 Gaps.v28,
                 TextFormField(
-                  decoration: const InputDecoration(
+                  cursorColor: Theme.of(context).primaryColor,
+                  decoration: InputDecoration(
                     hintText: "Email",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
                   ),
                   validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      return "Please enter your email";
+                    }
                     return null;
                   },
                   onSaved: (newValue) {
-                    formData["email"] = newValue!;
+                    if (newValue != null) {
+                      formData["email"] = newValue;
+                    }
                   },
                 ),
                 Gaps.v16,
                 TextFormField(
-                  decoration: const InputDecoration(
+                  decoration: InputDecoration(
                     hintText: "Password",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
                   ),
                   validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      return "Please enter your password";
+                    }
                     return null;
                   },
                   onSaved: (newValue) {
-                    formData["password"] = newValue!;
+                    if (newValue != null) {
+                      formData["password"] = newValue;
+                    }
                   },
                 ),
                 Gaps.v28,
