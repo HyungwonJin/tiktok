9f3b96b52226b2c6f829b01333a16371df9992b9
HyungwonJin <hwjin92@gmail.com>
2023-06-06 19:38:59 +0900

    feat: VisibilityDetector

--- lib/features/video/video_timeline_screen.dart
+++ lib/features/video/video_timeline_screen.dart
@@ -48,7 +48,7 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChanged,
       itemBuilder: (context, index) =>
-          VideoPost(onVideoFinished: _onVideoFinished),
+          VideoPost(onVideoFinished: _onVideoFinished, index: index),
     );
   }
 }
