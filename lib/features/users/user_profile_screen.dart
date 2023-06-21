bd3f9f8f8eeec0f20544ca74aaf08a082bb5f42b
HyungwonJin <hwjin92@gmail.com>
2023-06-21 11:49:57 +0900

    CustomScrollView

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -10,6 +10,32 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return const CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          backgroundColor: Colors.teal,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: [
+              StretchMode.zoomBackground,
+              StretchMode.blurBackground,
+            ],
+            title: Text('User Profile'),
+            background: Image(
+              image: AssetImage('assets/images/placeholder.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverFillRemaining(
+          child: Center(
+            child: Text('User Profile'),
+          ),
+        ),
+      ],
+    );
   }
 }
